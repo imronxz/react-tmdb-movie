eb5fd7e8a6c88c31c2cc7e1815daa77c63cfe727
imronxz <imronxz@gmail.com>
2022-09-28 20:40:37 +0700

    w

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'df7934f249ad19ef751a3c7ae05883e7';
+const API_KEY = 'd6648e6ca4b936ae2cab5db7c68f0466';
 const baseUrl = 'https://api.themoviedb.org/3/';
 
 const tmdb = axios.create({
